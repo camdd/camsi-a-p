d27ae7256447ec6f9eeaca68ad76ffce9874ef95
Cam <camila.dambrosio95@gmail.com>
2022-05-30 04:13:06 -0300

    CORS *

--- backend/src/main/java/com/camdportfolioap/ap/ApApplication.java
+++ backend/src/main/java/com/camdportfolioap/ap/ApApplication.java
@@ -19,7 +19,7 @@ public class ApApplication {
 	public CorsFilter corsFilter() {
 		CorsConfiguration corsConfiguration = new CorsConfiguration();
 		corsConfiguration.setAllowCredentials(true);
-		corsConfiguration.setAllowedOrigins(Arrays.asList("https://a-p-camd.web.app/"));
+		corsConfiguration.setAllowedOrigins(Arrays.asList("*"));
 		corsConfiguration.setAllowedHeaders(Arrays.asList("Origin", "Access-Control-Allow-Origin", "Content-Type",
 				"Accept", "Authorization", "Origin, Accept", "X-Requested-With",
 				"Access-Control-Request-Method", "Access-Control-Request-Headers"));
